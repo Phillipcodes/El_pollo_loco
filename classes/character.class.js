6e992529972ac31ec07c39e8ca60fcb896e71984
Phillipcodes <Phillipsauer00@gmail.com>
2024-07-24 09:51:55 +0200

    added sound in the audio folder

--- classes/character.class.js
+++ classes/character.class.js
@@ -5,7 +5,7 @@ class Character extends MovableObject {
     IMAGES_WALKING = ['../img/2_character_pepe/2_walk/W-21.png','../img/2_character_pepe/2_walk/W-22.png','../img/2_character_pepe/2_walk/W-23.png','../img/2_character_pepe/2_walk/W-24.png','../img/2_character_pepe/2_walk/W-25.png','../img/2_character_pepe/2_walk/W-26.png'];
     world;
     speed = 4;
-    walking_sound = new Audio('../pepe_walk_sound.mp3');
+    walking_sound = new Audio('../audio/pepe_walk_sound.mp3');
     
     constructor() {
     super().loadImage('../img/2_character_pepe/2_walk/W-21.png') 
